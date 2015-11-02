9e3fb2c24491356d2e636a6ef65c22d9086a4502
darwincito23 <darwincito23@gmail.com>
2015-11-02 17:26:44 -0500

    Versión 1.0
    Descripción:
    Proyecto Base Yii 2.0

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=distriguadua',
+    'username' => 'root',
+    'password' => '23dejulio',
+    'charset' => 'utf8',
+];
