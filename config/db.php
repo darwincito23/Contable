3e40dd5d1069a4cdaad1ed11e543572af1014561
darwincito23 <darwincito23@gmail.com>
2015-11-03 02:12:33 -0500

    Versión 1.0
    Descripción:
    Cambio Versión Framework 1.13

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=distriguadua',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
