cb207bbd38b2cb447611434f76f6e852cdfd4ef9
Lnesto <luis.nestomyl@gmail.com>
2015-11-04 13:05:39 -0500

    Versión 1.5
    Descripcion: Se realizó la configuración de las vistas del CRUD, del
    controlador y del modelo para Proveedores.

--- protected/config/main.php
+++ protected/config/main.php
@@ -24,7 +24,7 @@ return array(
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'23dejulio',
+			'password'=>'12345',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
@@ -58,7 +58,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=distriguadua',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '23dejulio',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
--- protected/views/proveedor/create.php
+++ protected/views/proveedor/create.php
@@ -8,11 +8,45 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Proveedor', 'url'=>array('index')),
-	array('label'=>'Manage Proveedor', 'url'=>array('admin')),
+	array('label'=>'Mostrar Proveedores', 'url'=>array('index')),
+	array('label'=>'Administrar Proveedores', 'url'=>array('admin')),
 );
 ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-1"></div>
+		<div class="col-md-6 text-center">
+			<h1>Crear Proveedor</h1>
+		</div>
+		<div class="col-md-3"></div>
+	</div>	
+	
+	<div class="col-md-1"></div>
+	<div class="col-md-6">
+		<contenido>
 
-<h1>Create Proveedor</h1>
+			<div class="panel panel-default">
+				  <div class="panel-heading">
+					<h4 class="text-center">Proveedores</h4>				  
+				  </div>
+				  <div class="panel-body">
+				   	<div class="row">
+					          	<div class="col-md-1">		          		
+					          	</div>
+					          	<div class="col-md-10">
+					
+									<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+														          		
+					          	</div>
+					          	<div class="col-md-1">        		
+				          		</div>
+					</div>	 
+				  </div>
+			</div>	
+				
+		</contenido>
+	</div>
+	<div class="col-md-5"></div>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
+
