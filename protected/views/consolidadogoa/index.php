b0092250391d293ddd0b5e798e5a3f0e5d948c2c
Lnesto <luis.nestomyl@gmail.com>
2015-11-11 21:17:19 -0500

    Version 1.9
    Se desarrollo el consolidado de GOA Y GOV.

--- protected/views/consolidadogoa/index.php
+++ protected/views/consolidadogoa/index.php
@@ -3,6 +3,23 @@
 /* @var $model ConsolidadoGoaGov */
 /* @var $form CActiveForm */
 ?>
+<?php Yii::app()->clientScript->registerScript('re-install-date-picker', "
+function reinstallDatePicker(id, data) {
+        //use the same parameters that you had set in your widget else the datepicker will be refreshed by default
+    $('#fecha_i').datepicker(jQuery.extend({'dateFormat':'yy-mm-dd'}));
+    $('#fecha_f').datepicker(jQuery.extend({'dateFormat':'yy-mm-dd'}));
+}
+");
+?>
+<?php Yii::app()->clientScript->registerScript('search', "
+$('#boton').submit(function(){
+	$('#consolidadogoagov-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
 <h1 class="text-center">Consolidado GOA/GOV</h1>
 <br>
 <div class="container-fluid">
@@ -24,9 +41,8 @@
 				        <tr class="tablacolor">
 							<td>	
 								<?php echo $form->labelEx($model,'fecha_Inicio'); ?>
-								<?php echo $form->textField($model,'fecha_Inicio'); ?>
 								<?php echo $form->error($model,'fecha_Inicio'); ?>
-								<?php  $this->widget('zii.widgets.jui.CJuiDatePicker',array(
+								<?php echo $this->widget('zii.widgets.jui.CJuiDatePicker',array(
 								      'model'=>$model,
 								      'attribute'=>'fecha_Inicio',
 								      // additional javascript options for the date picker plugin
@@ -35,16 +51,16 @@
 								          'dateFormat' => 'yy-mm-dd'
 								      ),
 								      'htmlOptions'=>array(
-								          'style'=>'height:20px;'
+								          'style'=>'height:20px;',
+								          'id'=>'fecha_i'
 								      ),
 								  ),true);
 								?>
 							</td>
 							<td>
 								<?php echo $form->labelEx($model,'fecha_fin'); ?>
-								<?php echo $form->textField($model,'fecha_fin'); ?>
 								<?php echo $form->error($model,'fecha_fin'); ?>	
-								<?php  $this->widget('zii.widgets.jui.CJuiDatePicker',array(
+								<?php echo $this->widget('zii.widgets.jui.CJuiDatePicker',array(
 								      'model'=>$model,
 								      'attribute'=>'fecha_fin',
 								      // additional javascript options for the date picker plugin
@@ -53,28 +69,67 @@
 								          'dateFormat' => 'yy-mm-dd'
 								      ),
 								      'htmlOptions'=>array(
-								          'style'=>'height:20px;'
+								          'style'=>'height:20px;',
+								          'id'=>'fecha_f',
 								      ),
 								  ),true);
 								?>			
 							</td>																		
 				        </tr> 
 				        <tr>
-				        	<td>
-								<?php echo $form->labelEx($model,'Cuenta'); ?>
-								<?php echo $form->textField($model,'RegistroContableGastos_idRegistroContableGastos'); ?>
-								<?php echo $form->error($model,'RegistroContableGastos_idRegistroContableGastos'); ?>			
-							</td>
-							<td>
-								<?php echo $form->labelEx($model,'Tercero'); ?>
-								<?php echo $form->textField($model,'RegistroContableGastos_idRegistroContableGastos'); ?>
-								<?php echo $form->error($model,'RegistroContableGastos_idRegistroContableGastos'); ?>
-							</td>					
+				        <td>
+				<?php echo $form->labelEx($model,'cuentac'); ?>
+								
+				<?php $this->widget('zii.widgets.jui.CJuiAutoComplete',
+          								array(
+           								  	'model'=>$model,
+      										'attribute'=>'cuentac',
+           								  	'source'=>Registrocontablegastos::model()->obtenerCodigoNombre('cuentapuc'),
+           								  	'options'=>array(
+        								  					'minLength'=>'1',
+        								  					'select'=>'js:function( event, ui ) {
+          								  					var valor=ui.item.value;
+          								  					var sp=valor.split("<->");
+          								  					$("#codigoc").val(sp[0]);               
+          								  					return true;  
+       								 						}'
+        								  					), //match case when performing a lookup?	
+            								 'htmlOptions'=>array('size'=>'40'), 
+            								 ));?>
+				<?php echo $form->error($model,'RegistroContableGastos_idRegistroContableGastos'); ?>
+				<?php echo $form->hiddenField($model,'codigoc',
+								array('id'=>'codigoc'
+								)); ?>			
+						</td>
+						<td>
+			    <?php echo $form->labelEx($model,'Proveedor'); ?>
+			    <?php $this->widget('zii.widgets.jui.CJuiAutoComplete',
+          								array(
+           								  	'model'=>$model,
+      										'attribute'=>'proveedorc',
+           								  	'source'=>Registrocontablegastos::model()->obtenerCodigoNombre('proveedor'),
+           								  	'options'=>array(
+        								  					'minLength'=>'1',
+        								  					'select'=>'js:function( event, ui ) {
+          								  					var valor=ui.item.value;
+          								  					var sp=valor.split("<->");
+          								  					$("#codigop").val(sp[0]);                
+          								  					return true;  
+       								 						}'
+        								  					), //match case when performing a lookup?	
+            								 'htmlOptions'=>array('size'=>'40'), 
+            								 ));?>
+
+				<?php echo $form->hiddenField($model,'codigop',
+								array('id'=>'codigop'
+								)); ?>					
+		    	<?php echo $form->error($model,'RegistroContableGastos_idRegistroContableGastos'); ?>
+						</td>					
 				        </tr>				        
 				    </tbody>		
 				</table>
 				<div class="row text-center">
-						<?php echo CHtml::submitButton('Buscar',array("class"=>"btn btn-success")); ?>
+						<?php echo CHtml::submitButton('Buscar',array("class"=>"btn btn-success",'id'=>'boton')); ?>
 				</div>
 			<?php $this->endWidget(); ?>
 		</div><!-- form -->
@@ -87,7 +142,40 @@
 	<div class="col-md-8">
 		<div class="row text-center">
 			<p>
-				espacio para el widget
+				<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'consolidadogoagov-grid',
+	'dataProvider'=>$model->searchAll($model->fecha_Inicio,
+										$model->fecha_fin,
+										$model->proveedorc!=''?Registrocontablegastos::model()->findId('proveedor',$model->codigop):'',
+										$model->cuentac!=''?Registrocontablegastos::model()->findId('cuentapuc',$model->codigoc):''),
+	'afterAjaxUpdate'=>'reinstallDatePicker',
+	'columns'=>array(
+		
+			array(
+				'name'=>'fecha',
+				'value'=>'$data->fecha',
+				'header'=>'Fecha',
+				),
+			array(
+				'name'=>'proveedor',
+				'value'=>'$data->proveedorIdProveedor->codigoProveedor.\'<->\'.$data->proveedorIdProveedor->nombreProveedor',
+				'header'=>'Proveedor',
+				),
+			array(
+				'name'=>'cuenta',
+				'value'=>'$data->cuentaPucIdCuentaPuc->codigoCuentaPuc.\'<->\'.$data->cuentaPucIdCuentaPuc->nombreCuentaPuc',
+				'header'=>'CuentaPuc',
+				),
+			array(
+				'name'=>'valor',
+				'value'=>'$data->valorRegistroContable',
+				'header'=>'Valor',
+				),
+	
+		),
+	));
+		
+	 ?>
 			</p>
 		</div>
 		<div class="row text-center">
