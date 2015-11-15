f21b648c9c65bc5b6dfded49375ac738e6500851
darwincito23 <darwincito23@gmail.com>
2015-11-14 19:48:58 -0500

    Versión 1.9.2

--- protected/views/frenteaprovechamientoproductos/create.php
+++ protected/views/frenteaprovechamientoproductos/create.php
@@ -33,39 +33,41 @@ $this->breadcrumbs=array(
 			</div>
 			<div class="col-md-2"></div>
 		</div><!--end row-->
-
-		<div class="row">		
-
+		<div class="col-md-3"></div>
+		<div class="row col-md-6">
 				<?php //echo $form->errorSummary($model); ?>
-
 				<div class="row">
-						<div class="col-md-4"></div>
-						<div class="col-md-2">	
-							<div class="row">
-								<?php echo $form->labelEx($model,'idProductos'); ?>
-							</div><!--end row-->
-							<div class="row">
-								<?php echo $form->textField($model,'Productos_idProductos'); ?>
-								<?php echo $form->error($model,'Productos_idProductos'); ?>
-							</div><!--end row-->								
-						</div>
-						<div class="col-md-2">
-							<div class="row">
-								<?php echo $form->labelEx($model,'CostoUnitario'); ?>
-							</div>	
-							<div class="row">
-								<?php echo $form->textField($model,'CostoUnitario'); ?>
-								<?php echo $form->error($model,'CostoUnitario'); ?>
-							</div>												
-						</div>
-						<div class="col-md-4"></div>
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<div class="col-md-1"></div>
+							<div class="col-md-5">							  
+							  		<div class="row">
+										<?php echo $form->labelEx($model,'idProductos'); ?>
+									</div><!--end row-->
+									<div class="row">
+										<?php echo $form->textField($model,'Productos_idProductos'); ?>
+										<?php echo $form->error($model,'Productos_idProductos'); ?>
+									</div><!--end row-->									
+							</div>
+							<div class="col-md-5">
+								<div class="row">
+									<?php echo $form->labelEx($model,'CostoUnitario'); ?>
+								</div>	
+								<div class="row">
+									<?php echo $form->textField($model,'CostoUnitario'); ?>
+									<?php echo $form->error($model,'CostoUnitario'); ?>
+								</div>												
+							</div>
+							<div class="col-md-1"></div>
+							</div>
+					</div>						
 				</div>	
 				<div class="row buttons">
-					<?php echo CHtml::submitButton('Guardar'); ?>
+					<?php echo CHtml::submitButton('Guardar',array('class'=>'btn btn-success btn-md')); ?>
 				</div>
-		</div>
+		</div><!--end row-->
+		<div class="col-md-3"></div>		
 		<?php $this->endWidget(); ?>
-
 	</div><!-- form -->
 </div>
 
